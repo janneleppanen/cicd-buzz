a09bfe160afffaa10e7269e030bd4fe80493e09b
Janne Leppänen <janne@janneleppanen.com>
2018-04-15 20:03:20 +0300

    Change docker path

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO
+
+docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
+docker push $DOCKER_PATH
